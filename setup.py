61bbbc78080187395429714f57ee715971a3c187
Shantanoo Desai <des@biba.uni-bremen.de>
2020-09-30 16:16:45 +0200

    bump v0.0.1 -> v0.0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='bnofluxlite',
-    version='0.0.1',
+    version='0.0.2',
     description='CLI to parse BNO055 IMU data and publish them via MQTT and store into InfluxDB',
     long_description=readme(),
     author='Shan Desai',
